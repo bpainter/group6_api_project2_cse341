3bcaa3f3a3159ac854ad3779b358cbc6dacaf5fc
adgar <adgar@adamspc>
2022-03-12 13:08:52 -0700

    create Schemas for User and Job

--- models/Job.js
+++ models/Job.js
@@ -2,7 +2,35 @@ const mongoose = require("mongoose");
 
 const JobSchema = new mongoose.Schema(
   {
-    // 
+    title: {
+      type: String,
+      required: true
+    },
+    employer: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    // details like salary, ft or pt, etc
+    details: {
+      type: String,
+      required: true
+    },
+    qualifications: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    datePosted: {
+      type: Date,
+      default: Date.now
+    }
   },
   { timestamps: true }
 );
