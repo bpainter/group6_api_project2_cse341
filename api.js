d28580ed5355a3b7afd8999cf47f535d1134a183
Bermon Painter <git@bermonpainter.com>
2022-04-04 21:11:34 -0400

    Cleaned up main api file, removed unecessary JS from project 1, made Mongo auth easier

--- api.js
+++ api.js
@@ -2,86 +2,51 @@
 // the environment variables for the API
 require('dotenv').config();
 
-const path = require('path');
 const express = require('express');
+const api = express();
+
+// Parse incoming request bodies in a middleware before your handlers, available under the req.body property
 const bodyParser = require('body-parser');
+
+// Connect to mongoDB
 const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csrf = require('csurf');
-const flash = require('connect-flash');
 
-const app = express();
-
-
-// routers
+// Routers for authentication & job CRUD
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 
-const store = new MongoDBStore({
-  uri: process.env.MONGODB_URI,
-  collection: 'sessions'
-});
-const csrfProtection = csrf();
+api.use(bodyParser.json());
 
-// Create the routes/
-// Follow a versioned API convention
-app.use("/api/v1/auth", authRouter);
-// app.use("/api/v1/jobs", authenticateUser, jobsRouter);
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(
-  session({
-    secret: 'my secret',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-  })
-);
-app.use(csrfProtection);
-app.use(flash());
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
+// Set headers for the REST API calls
+api.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
-app.use((req, res, next) => {
-  // throw new Error('Sync Dummy');
-//   if (!req.session.user) {
-//     return next();
-//   }
-//   User.findById(req.session.user._id)
-//     .then(user => {
-//       if (!user) {
-//         return next();
-//       }
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => {
-//       next(new Error(err));
-//     });
+// Set up paths for the versioned api
+api.use("/api/v1/auth", authRouter);
+api.use("/api/v1/jobs", jobsRouter);
+
+// Handle errors
+api.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
-// app.get('/500', errorController.get500);
-// app.use(errorController.get404);
-// app.use((error, req, res, next) => {
-//   // res.status(error.httpStatusCode).render(...);
-//   // res.redirect('/500');
-//   res.status(500).render('500', {
-//     pageTitle: 'Error!',
-//     path: '/500',
-//     isAuthenticated: req.session.isLoggedIn
-//   });
-// });
-
+// Connect to MonboDB
+// Environment info are stores in the .env file
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(result => {
-    app.listen(process.env.PORT, () => {
+    api.listen(process.env.PORT, () => {
       console.log(`Job API up and running successfully`);
       console.log(`Server listening on ${process.env.PORT}`);
     });
